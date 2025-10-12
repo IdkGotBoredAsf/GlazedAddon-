1ad6906a42a20cfa15594f7f8c0f8d08595f1d3f
IdkGotBoredAsf <pyraclient@gmail.com>
2025-10-12 09:29:18 +0100

    GlazedAddon.java

--- src/main/java/com/nnpg/glazed/GlazedAddon.java
+++ src/main/java/com/nnpg/glazed/GlazedAddon.java
@@ -64,7 +64,7 @@ public class GlazedAddon extends MeteorAddon {
         Modules.get().add(new LegitAnchorMacro());
         Modules.get().add(new AutoPotion());
         Modules.get().add(new SpawnerOrder());
-        Modules.get().add(new Freecam());
+        Modules.get().add(new FreecamV2());
         Modules.get().add(new RegionMap());
         Modules.get().add(new HoverTotem());
 
