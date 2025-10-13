6b613a270e25c38c5ba5dc4741bcb171b5d3f67e
IdkGotBoredAsf <pyraclient@gmail.com>
2025-10-13 17:18:00 +0100

    SusESP.java

--- src/main/java/com/nnpg/glazed/modules/esp/SusESP.java
+++ src/main/java/com/nnpg/glazed/modules/esp/SusESP.java
@@ -8,7 +8,6 @@ import meteordevelopment.meteorclient.utils.render.RenderUtils;
 import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.math.*;
 import net.minecraft.world.chunk.WorldChunk;
@@ -20,25 +19,25 @@ public class SusESP extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
     private final Setting<SettingColor> color = sgGeneral.add(new ColorSetting.Builder()
-        .name("highlight-color")
-        .description("Color of the ESP box and tracer.")
-        .defaultValue(new SettingColor(180, 90, 255, 180)) // soft purple
-        .build()
+            .name("highlight-color")
+            .description("Color of the ESP box and tracer.")
+            .defaultValue(new SettingColor(180, 90, 255, 180))
+            .build()
     );
 
     private final Setting<Boolean> showTracers = sgGeneral.add(new BoolSetting.Builder()
-        .name("show-tracers")
-        .description("Draws a tracer from crosshair to detected blocks.")
-        .defaultValue(true)
-        .build()
+            .name("show-tracers")
+            .description("Draws a tracer from crosshair to detected blocks.")
+            .defaultValue(true)
+            .build()
     );
 
     private final Setting<Double> tracerThickness = sgGeneral.add(new DoubleSetting.Builder()
-        .name("tracer-thickness")
-        .defaultValue(1.2)
-        .min(0.1)
-        .sliderMax(5)
-        .build()
+            .name("tracer-thickness")
+            .defaultValue(1.2)
+            .min(0.1)
+            .sliderMax(5)
+            .build()
     );
 
     private final Map<ChunkPos, BlockPos> espTargets = new HashMap<>();
@@ -51,8 +50,8 @@ public class SusESP extends Module {
     private void onRender3D(Render3DEvent event) {
         if (mc.world == null) return;
 
-        // Update detected chunks
-        for (WorldChunk chunk : mc.world.getChunkManager().getChunks().values()) { // Fixed
+        // Iterate loaded chunks properly in 1.21
+        for (WorldChunk chunk : mc.world.getChunkManager().getLoadedChunksIterable()) {
             ChunkPos pos = chunk.getPos();
             if (!espTargets.containsKey(pos)) {
                 BlockPos found = findRotatedDeepslate(chunk);
@@ -63,23 +62,22 @@ public class SusESP extends Module {
             }
         }
 
-        MatrixStack matrices = event.matrices; // Fixed
+        MatrixStack matrices = event.matrices;
         SettingColor c = color.get();
         float r = c.r / 255f;
         float g = c.g / 255f;
         float b = c.b / 255f;
         float a = c.a / 255f;
 
-        RenderSystem.disableDepthTest();
-
         for (BlockPos bp : espTargets.values()) {
             if (bp == null) continue;
-            Box box = new Box(bp);
-            RenderUtils.boxESP(matrices, box, c, 1.0f, true, true, true, true); // Fixed
-            if (showTracers.get()) drawTracerToBlock(matrices, bp, r, g, b, a);
-        }
 
-        RenderSystem.enableDepthTest();
+            // Draw ESP box using Meteor's RenderUtils
+            RenderUtils.box(matrices, new Box(bp), r, g, b, a, (float) tracerThickness.get());
+
+            // Draw tracers
+            if (showTracers.get()) drawTracer(matrices, bp, r, g, b, a);
+        }
     }
 
     private BlockPos findRotatedDeepslate(WorldChunk chunk) {
@@ -91,7 +89,7 @@ public class SusESP extends Module {
                 for (int z = 0; z < 16; z++) {
                     BlockPos pos = new BlockPos(startX + x, y, startZ + z);
                     if (mc.world.getBlockState(pos).getBlock() == Blocks.DEEPSLATE
-                        && mc.world.getBlockState(pos).getEntries().toString().contains("axis")) {
+                            && mc.world.getBlockState(pos).getEntries().toString().contains("axis")) {
                         return pos;
                     }
                 }
@@ -107,22 +105,11 @@ public class SusESP extends Module {
         return new BlockPos(x, y, z);
     }
 
-    private void drawTracerToBlock(MatrixStack matrices, BlockPos pos, float r, float g, float b, float a) {
+    private void drawTracer(MatrixStack matrices, BlockPos pos, float r, float g, float b, float a) {
         Vec3d camPos = mc.gameRenderer.getCamera().getPos();
         Vec3d blockCenter = Vec3d.ofCenter(pos);
-        Vec3d crosshair = camPos.add(mc.player.getRotationVector());
 
-        RenderSystem.setShader(GameRenderer.getPositionColorShader()); // Fixed
-        RenderSystem.lineWidth(tracerThickness.get().floatValue());
-
-        Tessellator tess = Tessellator.getInstance();
-        BufferBuilder buffer = tess.getBuffer();
-        buffer.begin(VertexFormat.DrawMode.LINES, VertexFormats.POSITION_COLOR);
-
-        // Cast doubles to floats
-        buffer.vertex((float) crosshair.x, (float) crosshair.y, (float) crosshair.z).color(r, g, b, a).next();
-        buffer.vertex((float) blockCenter.x, (float) blockCenter.y, (float) blockCenter.z).color(r, g, b, a).next();
-
-        tess.draw();
+        // Use Meteor RenderUtils line for tracers
+        RenderUtils.line(matrices, camPos, blockCenter, r, g, b, a, (float) tracerThickness.get());
     }
 }
