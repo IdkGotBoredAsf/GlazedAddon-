5e4631fe9f3e6b82d8cdc191d11a11acedcba4ce
IdkGotBoredAsf <pyraclient@gmail.com>
2025-10-13 16:27:03 +0100

    SusESP.java

--- src/main/java/com/nnpg/glazed/modules/esp/SusESP.java
+++ src/main/java/com/nnpg/glazed/modules/esp/SusESP.java
@@ -1,145 +1,169 @@
 package com.nnpg.glazed.modules.esp;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.nnpg.glazed.GlazedAddon;
 import meteordevelopment.meteorclient.events.render.Render3DEvent;
-import meteordevelopment.meteorclient.renderer.ShapeMode;
 import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.utils.render.color.Color;
-import meteordevelopment.meteorclient.utils.render.color.SettingColor;
 import meteordevelopment.orbit.EventHandler;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.client.render.*;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.*;
+import net.minecraft.world.chunk.WorldChunk;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.awt.*;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static meteordevelopment.meteorclient.utils.render.color.ColorUtils.*;
 
 public class SusESP extends Module {
-    private final SettingGroup sgGeneral = settings.createGroup("General");
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    private final Setting<Double> range = sgGeneral.add(new DoubleSetting.Builder()
-        .name("range")
-        .description("How far to search for suspicious blocks.")
-        .defaultValue(50)
-        .min(5)
-        .max(200)
-        .sliderMax(200)
+    private final Setting<Color> color = sgGeneral.add(new ColorSetting.Builder()
+        .name("highlight-color")
+        .description("Color of the ESP box and tracer.")
+        .defaultValue(new Color(180, 90, 255, 180)) // soft purple
         .build()
     );
 
-    private final Setting<Boolean> tracers = sgGeneral.add(new BoolSetting.Builder()
-        .name("tracers")
-        .description("Draw tracers to suspicious blocks.")
+    private final Setting<Boolean> showTracers = sgGeneral.add(new BoolSetting.Builder()
+        .name("show-tracers")
         .defaultValue(true)
         .build()
     );
 
-    private final Setting<ShapeMode> shapeMode = sgGeneral.add(new EnumSetting.Builder<ShapeMode>()
-        .name("shape-mode")
-        .description("How boxes are rendered.")
-        .defaultValue(ShapeMode.Both)
+    private final Setting<Double> tracerThickness = sgGeneral.add(new DoubleSetting.Builder()
+        .name("tracer-thickness")
+        .defaultValue(1.2)
+        .min(0.1)
+        .sliderMax(5)
         .build()
     );
 
-    private final Setting<SettingColor> color = sgGeneral.add(new ColorSetting.Builder()
-        .name("color")
-        .description("Color of the boxes.")
-        .defaultValue(new SettingColor(255, 100, 0, 150))
-        .build()
-    );
-
-    private final Setting<Boolean> clusterDetection = sgGeneral.add(new BoolSetting.Builder()
-        .name("cluster-detection")
-        .description("Only render clusters of suspicious blocks close together (helps remove false positives).")
-        .defaultValue(true)
-        .build()
-    );
-
-    private final Setting<Integer> clusterRadius = sgGeneral.add(new IntSetting.Builder()
-        .name("cluster-radius")
-        .description("How close blocks must be to be considered a cluster.")
-        .defaultValue(3)
-        .min(1)
-        .max(10)
-        .visible(clusterDetection::get)
-        .build()
-    );
-
-    private final Set<Block> suspiciousBlocks = Set.of(
-        Blocks.CHEST, Blocks.TRAPPED_CHEST,
-        Blocks.ENDER_CHEST,
-        Blocks.FURNACE, Blocks.BLAST_FURNACE, Blocks.SMOKER,
-        Blocks.CRAFTING_TABLE,
-        Blocks.ANVIL, Blocks.CHIPPED_ANVIL, Blocks.DAMAGED_ANVIL,
-        Blocks.BEDROCK, // sometimes used in custom builds or markers
-        Blocks.BED,
-        Blocks.ENCHANTING_TABLE,
-        Blocks.BREWING_STAND
-    );
-
-    private final Set<BlockPos> renderPositions = new HashSet<>();
+    private final Map<ChunkPos, BlockPos> espTargets = new HashMap<>();
 
     public SusESP() {
-        super(GlazedAddon.esp, "sus-esp", "Highlights suspicious blocks that could indicate a base.");
+        super(GlazedAddon.esp, "sus-esp", "Highlights random blocks in chunks where rotated deepslate is detected.");
     }
 
     @EventHandler
     private void onRender3D(Render3DEvent event) {
-        if (mc.player == null || mc.world == null) return;
-        renderPositions.clear();
+        if (mc.world == null) return;
 
-        BlockPos playerPos = mc.player.getBlockPos();
-        int r = range.get().intValue();
+        for (WorldChunk chunk : mc.world.getChunkManager().getLoadedChunksIterable()) {
+            ChunkPos pos = chunk.getPos();
 
-        // Scan nearby blocks
-        for (int x = -r; x <= r; x++) {
-            for (int y = -r; y <= r; y++) {
-                for (int z = -r; z <= r; z++) {
-                    BlockPos pos = playerPos.add(x, y, z);
+            if (!espTargets.containsKey(pos)) {
+                BlockPos found = findRotatedDeepslate(chunk);
+                if (found != null) {
+                    // Choose a random block in this chunk
+                    BlockPos random = pickRandomBlockInChunk(pos);
+                    espTargets.put(pos, random);
+                }
+            }
+        }
+
+        MatrixStack matrices = event.matrices();
+        RenderSystem.disableDepthTest();
+        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+        Tessellator tess = Tessellator.getInstance();
+        BufferBuilder buffer = tess.getBuffer();
+
+        Color c = color.get();
+        float r = c.getRed() / 255f;
+        float g = c.getGreen() / 255f;
+        float b = c.getBlue() / 255f;
+        float a = c.getAlpha() / 255f;
+
+        for (BlockPos bp : espTargets.values()) {
+            if (bp == null) continue;
+            Box box = new Box(bp);
+            RenderUtils.drawBoxOutline(box, r, g, b, a, (float) tracerThickness.get());
+            if (showTracers.get()) drawTracerToBlock(bp, r, g, b, a);
+        }
+
+        RenderSystem.enableDepthTest();
+    }
+
+    private BlockPos findRotatedDeepslate(WorldChunk chunk) {
+        int startX = chunk.getPos().getStartX();
+        int startZ = chunk.getPos().getStartZ();
+
+        for (int x = 0; x < 16; x++) {
+            for (int y = -64; y <= 45; y++) {
+                for (int z = 0; z < 16; z++) {
+                    BlockPos pos = new BlockPos(startX + x, y, startZ + z);
                     Block block = mc.world.getBlockState(pos).getBlock();
-
-                    if (suspiciousBlocks.contains(block)) {
-                        if (clusterDetection.get()) {
-                            if (!isIsolated(pos)) renderPositions.add(pos);
-                        } else {
-                            renderPositions.add(pos);
+                    if (block == Blocks.DEEPSLATE) {
+                        // Detect rotation-like state (pseudo check since rotation is stored in BlockState properties)
+                        if (mc.world.getBlockState(pos).getProperties().toString().contains("axis")) {
+                            return pos;
                         }
                     }
                 }
             }
         }
-
-        Vec3d playerCam = mc.player.getCameraPosVec(event.tickDelta);
-
-        for (BlockPos pos : renderPositions) {
-            Box box = new Box(pos);
-            Color c = new Color(color.get());
-            event.renderer.box(box, c, c, shapeMode.get(), 0);
-
-            if (tracers.get()) {
-                Vec3d center = new Vec3d(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
-                event.renderer.line(playerCam.x, playerCam.y, playerCam.z, center.x, center.y, center.z, c);
-            }
-        }
+        return null;
     }
 
-    private boolean isIsolated(BlockPos pos) {
-        int nearby = 0;
-        for (int dx = -clusterRadius.get(); dx <= clusterRadius.get(); dx++) {
-            for (int dy = -clusterRadius.get(); dy <= clusterRadius.get(); dy++) {
-                for (int dz = -clusterRadius.get(); dz <= clusterRadius.get(); dz++) {
-                    BlockPos neighbor = pos.add(dx, dy, dz);
-                    if (neighbor.equals(pos)) continue;
-                    if (suspiciousBlocks.contains(mc.world.getBlockState(neighbor).getBlock())) {
-                        nearby++;
-                        if (nearby >= 2) return false; // Found cluster
-                    }
-                }
-            }
+    private BlockPos pickRandomBlockInChunk(ChunkPos chunkPos) {
+        int x = ThreadLocalRandom.current().nextInt(16) + chunkPos.getStartX();
+        int z = ThreadLocalRandom.current().nextInt(16) + chunkPos.getStartZ();
+        int y = mc.world.getTopY(); // somewhere near surface
+        return new BlockPos(x, y, z);
+    }
+
+    private void drawTracerToBlock(BlockPos pos, float r, float g, float b, float a) {
+        Vec3d camPos = mc.gameRenderer.getCamera().getPos();
+        Vec3d blockCenter = Vec3d.ofCenter(pos);
+        Vec3d crosshair = camPos.add(mc.player.getRotationVector().multiply(1));
+
+        Tessellator tess = Tessellator.getInstance();
+        BufferBuilder buffer = tess.getBuffer();
+        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+
+        buffer.begin(VertexFormat.DrawMode.LINES, VertexFormats.POSITION_COLOR);
+        buffer.vertex(crosshair.x, crosshair.y, crosshair.z).color(r, g, b, a).next();
+        buffer.vertex(blockCenter.x, blockCenter.y, blockCenter.z).color(r, g, b, a).next();
+        tess.draw();
+    }
+
+    // Simple internal helper class for outlines
+    private static class RenderUtils {
+        static void drawBoxOutline(Box box, float r, float g, float b, float a, float thickness) {
+            RenderSystem.lineWidth(thickness);
+            Tessellator tess = Tessellator.getInstance();
+            BufferBuilder buffer = tess.getBuffer();
+            RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+
+            buffer.begin(VertexFormat.DrawMode.LINES, VertexFormats.POSITION_COLOR);
+
+            double x1 = box.minX, y1 = box.minY, z1 = box.minZ;
+            double x2 = box.maxX, y2 = box.maxY, z2 = box.maxZ;
+
+            // 12 edges of a box
+            add(buffer, x1, y1, z1, x2, y1, z1, r, g, b, a);
+            add(buffer, x1, y1, z1, x1, y2, z1, r, g, b, a);
+            add(buffer, x1, y1, z1, x1, y1, z2, r, g, b, a);
+            add(buffer, x2, y1, z1, x2, y2, z1, r, g, b, a);
+            add(buffer, x2, y1, z1, x2, y1, z2, r, g, b, a);
+            add(buffer, x1, y2, z1, x2, y2, z1, r, g, b, a);
+            add(buffer, x1, y1, z2, x2, y1, z2, r, g, b, a);
+            add(buffer, x1, y1, z2, x1, y2, z2, r, g, b, a);
+            add(buffer, x1, y2, z2, x2, y2, z2, r, g, b, a);
+            add(buffer, x2, y1, z2, x2, y2, z2, r, g, b, a);
+            add(buffer, x1, y2, z1, x1, y2, z2, r, g, b, a);
+            add(buffer, x2, y2, z1, x2, y2, z2, r, g, b, a);
+
+            tess.draw();
         }
-        return true;
+
+        private static void add(BufferBuilder buffer, double x1, double y1, double z1, double x2, double y2, double z2, float r, float g, float b, float a) {
+            buffer.vertex(x1, y1, z1).color(r, g, b, a).next();
+            buffer.vertex(x2, y2, z2).color(r, g, b, a).next();
+        }
     }
 }
